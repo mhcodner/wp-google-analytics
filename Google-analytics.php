186238f31894e3e328f053a3bcd973086c831f7d
mhcodner <mhcodner@gmail.com>
2015-09-17 13:35:44 +1000

    Initial commit

---
+++ Google-analytics.php
@@ -0,0 +1,13 @@
+<?php
+
+/*
+Plugin Name: Google Analytics by MhCodner
+Plugin URI: https://github.com/mhcodner/wp-google-analytics
+Description: Outputs necessary javascript for Google Analytics
+Version: 1.0
+Author: Michael Codner
+Author URI: http://mhcodner.me
+License: GPL2
+Text Domain: mc-google-analytics
+*/
+
